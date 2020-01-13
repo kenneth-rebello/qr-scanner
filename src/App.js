5d6c8a46b8ab8f4a4ae339586d622ec605c54936
Kenneth Rebello <54711676+kenneth-rebello@users.noreply.github.com>
2020-01-13 21:47:48 +0530

    Notif test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,50 @@ import Generator from './components/Generator';
 
 const App = () => {
 
+  const pushServerPublicKey = "BIN2Jc5Vmkmy-S3AUrcMlpKxJpLeVRAfu9WBqUbJ70SJOCWGCGXKY-Xzyh7HDr6KbRDGYHjqZ06OcS3BjD7uAm8";
+  const isPushNotificationSupported = () => {
+    return "serviceWorker" in navigator && "PushManager" in window;
+  }
+
+  const registerServiceWorker = () => {
+    return navigator.serviceWorker.register("/sw.js");
+  }
+
+  const askUserPermission = async() => {
+    return await Notification.requestPermission();
+  }
+
+  async function createNotificationSubscription() {
+    //wait for service worker installation to be ready
+    const serviceWorker = await navigator.serviceWorker.ready;
+    // subscribe and return the subscription
+    return await serviceWorker.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: pushServerPublicKey
+    });
+  }
+
+  function sendNotification() {
+    console.log('Here')
+    const img = "/images/jason-leung-HM6TMmevbZQ-unsplash.jpg";
+    const text = "Take a look at this brand new t-shirt!";
+    const title = "New Product Available";
+    const options = {
+      body: text,
+      // icon: "/images/jason-leung-HM6TMmevbZQ-unsplash.jpg",
+      // vibrate: [200, 100, 200],
+      // tag: "new-product",
+      // image: img,
+      // badge: "https://spyna.it/icons/android-icon-192x192.png",
+      // actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
+    };
+    navigator.serviceWorker.ready.then(function(serviceWorker) {
+      serviceWorker.showNotification(title, options);
+    });
+  }
+
+  sendNotification();
+
   return (
     <Router basename="/">
       <div>
